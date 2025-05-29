340adcd6b53a7da07255fcebbeffe1059d6aad73
trisaxparis <jp@riant.fr>
2025-05-29 13:57:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Annotation biais", layout="centered")
 
 # Chargement des fichiers nécessaires
 titre_path = "titres_manipulatifs10.csv"
-biais_path = "biais_complet_final_questions.csv"
+biais_path = "biais_complet_avec_questions.csv"
 
 if not os.path.exists(titre_path) or not os.path.exists(biais_path):
     st.error("Fichiers manquants. Vérifie la présence de 'titres_manipulatifs10.csv' et 'biais_complet_final_questions.csv'.")
